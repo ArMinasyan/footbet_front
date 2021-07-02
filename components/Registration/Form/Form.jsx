e4cbc4234d2191b5ca4e689c3f0c4a4baaaace42
trener <ermanfantasy@gmail.com>
2021-07-02 18:08:19 +0400

    install sass

--- components/Registration/Form/Form.jsx
+++ components/Registration/Form/Form.jsx
@@ -1,7 +1,21 @@
-export function Form() {
+//components
+import { InputConteiner } from './InputConteiner/InputConteiner'
+// styles
+import styles from './Form.module.scss'
+
+export function Form({ title }) {
     return (
-        <div>
-            Forma
+        <div className={styles.conteiner}>
+            <div>
+                <h1>
+                    {
+                        title
+                    }
+                </h1>
+            </div>
+            <form action="">
+                <InputConteiner />
+            </form>
         </div>
     )
 }
