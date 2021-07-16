52e3ce7bb16aaa561e29462cf61a5fda17139a0d
Erik <ermanfantasy@gmail.com>
2021-07-16 14:42:40 +0400

    Ball in footer fixed

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,9 +1,12 @@
+//
+import { Ball } from './Ball/Ball'
+// styles
 import styles from './Footer.module.scss'
 
 export function Footer() {
     return (
-        <div style={{height:'100vh'}}>
-            Footer
+        <div className={styles.container}>
+            <Ball />
         </div>
     )
 }
