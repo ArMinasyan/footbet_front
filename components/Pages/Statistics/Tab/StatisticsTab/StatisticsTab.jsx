6ef8a16f718684352b9568493d61e1448f183432
Erik <ermanfantasy@gmail.com>
2021-07-22 13:37:03 +0400

    Statistics page fixed

---
+++ components/Pages/Statistics/Tab/StatisticsTab/StatisticsTab.jsx
@@ -0,0 +1,32 @@
+import { Row } from '../Row/Row'
+import { TabHeader } from '../TabHeader/TabHeader';
+import { statistics_tab } from './statistics_tab';
+// styles 
+import styles from './StatisticsTab.module.scss'
+
+export function StatisticsTab() {
+    return (
+        <div className={styles.container}>
+            <TabHeader />
+            <div className={styles.board}>
+                {
+                    statistics_tab.map((el, i) => (
+                        <Row
+                            order={i + 1}
+                            teamOneName={el.teamOneName}
+                            teamOneIcon={el.teamOneIcon}
+                            teamTwoName={el.teamTwoName}
+                            teamTwoIcon={el.teamTwoIcon}
+                            date={el.date}
+                            time={el.time}
+                            prediction={el.prediction}
+                            score={el.score}
+                            gameResult={el.gameResult}
+                            key={Math.random()}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
