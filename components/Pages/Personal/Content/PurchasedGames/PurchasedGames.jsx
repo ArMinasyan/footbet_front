909f46837664d823a76f3fd4650aa8758192b83c
Erik <ermanfantasy@gmail.com>
2021-07-22 19:50:20 +0400

    Personal page ready for markup

---
+++ components/Pages/Personal/Content/PurchasedGames/PurchasedGames.jsx
@@ -0,0 +1,7 @@
+export function PurchasedGames() {
+    return (
+        <div>
+            Games
+        </div>
+    )
+}
--- components/Pages/Personal/Personal.jsx
+++ components/Pages/Personal/Personal.jsx
@@ -1,7 +1,20 @@
+// components
+import { Title } from '../common/Title/Title'
+import { Content } from './Content/Content'
+// styles 
+import styles from './Personal.module.scss'
+
+
 export default function Personal() {
     return (
-        <div>
-            Personal page
+        <div className={styles.container}>
+            <Title
+                page='personal'
+                textPathName='title'
+            />
+            <div className={styles.content}>
+                <Content />
+            </div>
         </div>
     )
 }
