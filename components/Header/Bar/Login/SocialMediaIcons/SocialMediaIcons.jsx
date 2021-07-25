9e05fb7209f4b6867013b354d28e2cf1d71b08f4
Erik <ermanfantasy@gmail.com>
2021-07-25 19:02:57 +0400

    google and facebook auth

--- components/Header/Bar/Login/SocialMediaIcons/SocialMediaIcons.jsx
+++ components/Header/Bar/Login/SocialMediaIcons/SocialMediaIcons.jsx
@@ -5,6 +5,9 @@ import vk from '/public/images/header/login/vk.svg'
 import twitter from '/public/images/header/login/twitter.svg'
 // scss
 import styles from './SocialMediaIcons.module.scss'
+import ReactFacebookLogin from 'react-facebook-login'
+import { useRef } from 'react'
+import GoogleLogin from 'react-google-login'
 
 const icons = [
     {
@@ -25,17 +28,74 @@ const icons = [
     }]
 
 
+
+const responseFacebook = (response) => {
+    console.log(response);
+}
+
+const responseGoogle = (response) => {
+    console.log(response);
+}
+
+
 export function SocialMediaIcons() {
+
+    function onSocialMediaBtnClick(i) {
+        if (i === 0) {
+            const
+                facebookLoginBtn = document.querySelector(`.facebook-login-btn`)
+            if (facebookLoginBtn)
+                facebookLoginBtn.click();
+        }
+        else if (i === 1) {
+            const
+                googleLoginBtn = document.querySelector(`.google-login-btn`)
+            if (googleLoginBtn)
+                googleLoginBtn.click();
+        }
+    }
+
     return (
         <div className={styles.conteiner}>
             <div className={styles.content}>
                 {
-                    icons.map(el => {
+                    icons.map((el, i) => {
                         return (
                             <div className={styles.iconContent} key={Math.random() * 10}>
-                                <a href={el.link} target="_blank">
+                                <a target="_blank">
                                     <div className={styles.iconBody}>
-                                        <img src={el.iconUrl} alt="" className={styles.icon} />
+                                        <img
+                                            src={el.iconUrl}
+                                            alt="" onClick={() => onSocialMediaBtnClick(i)}
+                                            className={styles.icon}
+                                            style={{ cursor: 'pointer' }}
+                                        />
+                                        {
+                                            (i === 0) && (
+                                                <ReactFacebookLogin
+                                                    containerStyle={{ display: `none` }}
+                                                    cssClass="facebook-login-btn"
+                                                    appId="118863770361683"
+                                                    fields="name,email,picture"
+                                                    callback={responseFacebook} />
+                                            ),
+                                            (i === 1) && (
+                                                <GoogleLogin
+                                                    clientId="1089618047943-r91su9qkc0c9npo1tfoff3sp8421bt5g.apps.googleusercontent.com"
+                                                    render={renderProps => (
+                                                        <button
+                                                            onClick={renderProps.onClick}
+                                                            disabled={renderProps.disabled}
+                                                            style={{ display: 'none' }}
+                                                            className="google-login-btn"
+                                                        />)}
+                                                    cookiePolicy={'single_host_origin'}
+                                                    onSuccess={responseGoogle}
+                                                    onFailure={responseGoogle}
+                                                />
+                                            )
+
+                                        }
                                     </div>
                                 </a>
                             </div>
@@ -46,3 +106,6 @@ export function SocialMediaIcons() {
         </div>
     )
 }
+
+// TODO
+// add foot footbet domain in google develpors account
