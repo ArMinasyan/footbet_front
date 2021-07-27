c11a75037c822f0026c608bc6dca025dcfc5f353
carvus <armen.demirchyan.ait@gmail.com>
2021-07-27 14:30:50 -0700

    header responsive design

---
+++ components/Header/Bar/Dropdown/Dropdown.jsx
@@ -0,0 +1,126 @@
+// hooks and helpers
+import { useRouter } from 'next/dist/client/router'
+import { useState } from 'react';
+
+
+// styles
+import styles from './Dropdown.module.scss';
+
+// translation 
+import useTranslation from 'next-translate/useTranslation'
+
+//imgs
+import dropdownLines from '/public/images/header/dropdown_lines.png';
+
+//components
+import Link from 'next/link'
+import ContactsModal  from '../../../Pages/Contacts/ContactsModal';
+
+
+export default function Dropdown() {
+    const
+        [showDropdown, setShowDropdown] = useState(false),
+        [showContactsModal, setShowContactsModal] = useState(false),
+        { t } = useTranslation('common'),
+        translationPath = 'header.navBar.',
+        router = useRouter(),
+        translate = (key) => t(`${translationPath}${key}`),
+        buttonsInHomePage = [
+            {
+                key: Math.random(),
+                frstContent: translate('main'),
+                href: '/'
+            },
+            {
+                key: Math.random(),
+                frstContent: translate('games'),
+                href: '/games'
+            },
+            {
+                key: Math.random(),
+                frstContent: translate('statistics.frs'),
+                secContent: translate('statistics.sec'),
+                href: '/statistics'
+            },
+            {
+                key: Math.random(),
+                frstContent: translate('prediction.frs'),
+                secContent: translate('prediction.sec'),
+                href: '/prediction'
+            },
+            {
+                key: Math.random(),
+                frstContent: translate('testimonials'),
+                href: '/testimonials'
+            },
+            {
+                key: Math.random(),
+                frstContent: translate('aboutUs'),
+                href: '/aboutUs'
+            },
+            {
+                key: Math.random(),
+                frstContent: translate('contacts'),
+                href: null,
+                click: () => setShowContactsModal(true)
+            },
+        ],
+        buttons = [
+            {
+                key: Math.random(),
+                frstContent: translate('main'),
+                href: '/'
+            },
+            {
+                key: Math.random(),
+                frstContent: translate('games'),
+                href: '/games'
+            },
+            {
+                key: Math.random(),
+                frstContent: translate('prediction.frs'),
+                secContent: translate('prediction.sec'),
+                href: '/prediction'
+            },
+            {
+                key: Math.random(),
+                frstContent: translate('testimonials'),
+                href: '/testimonials'
+            },
+            {
+                key: Math.random(),
+                frstContent: translate('aboutUs'),
+                href: '/aboutUs'
+            },
+            {
+                key: Math.random(),
+                frstContent: translate('contacts'),
+                href: null,
+                click: () => setShowContactsModal(true)
+            },
+        ]
+    return (
+        <div className={styles.dropdown}>
+            <div className={styles.dropdown_check}>
+                <img src={dropdownLines.src} alt="" onClick={() => setShowDropdown(!showDropdown)} />
+            </div>
+            {showDropdown && (
+                <div className={styles.dropdown_menu}>
+                    <ul className={styles.dropdown_list}>
+                        {
+                            (router.pathname === '/' ? buttonsInHomePage : buttons).map(el => (
+                                <Link href={el.href ? el.href : ""}><li onClick={el.click && el.click} key={el.key} className={styles.dropdown_list_items}>{el.frstContent} {el.secContent}</li></Link>
+                            ))
+                        }
+                    </ul>
+                </div>
+            )}
+            {showContactsModal &&
+                <ContactsModal
+                    opened={showContactsModal ? true : false}
+                    onModalClose={() => setShowContactsModal(false)}
+                />
+            }
+        </div>
+    )
+}
