fd792e67333319bb3e057fcee197b7f68dcbb3b1
Arman <vip.arman.minasyan.197@gmail.com>
2021-08-10 23:08:54 +0400

    Routes are changed

---
+++ .next/static/Te05pxzLRc25TFJXvtz0M/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fregistration","\u002Fprediction","\u002Fstatistics","\u002Fprivacy-policy","\u002Ftestimonials","\u002Fpersonal","\u002FaboutUs","\u002F","\u002Fgames","\u002Fmatches"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
