c3b0d7670a6179c036e8794806598ac0290dbdb9
nothacker <rafayel.jamalyan@gmail.com>
2021-09-05 15:18:02 +0400

    rm cache

--- .next/static/cH-gYAScnO-9fLTNh34HP/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002FaboutUs","\u002Fpersonal","\u002Fregistration","\u002Fprediction","\u002Fstatistics","\u002Fterms-of-use","\u002Fprivacy-policy","\u002Ftestimonials","\u002Fgames","\u002Fmatches","\u002F"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
