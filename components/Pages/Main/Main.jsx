ce97c307864c6ed8e617ece5f33f97290a4a272f
nothacker <rafayel.jamalyan@gmail.com>
2021-09-21 01:16:53 +0400

    m

--- components/Pages/Main/Main.jsx
+++ components/Pages/Main/Main.jsx
@@ -1,11 +1,20 @@
 // components
+import { useEffect } from 'react'
 import { Testimonials } from '../common/Testimonials/Testimonials'
 import { Matches } from './Matches/Matches'
 import { News } from './News/News'
 import { Prediction } from './Prediction/Prediction'
+import { useSession } from 'next-auth/client'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../../../redux/features/userSlice'
 
 
 export default function Main() {
+  const { data: session } = useSession()
+  const user = useSelector(selectUser)
+  useEffect(()=>{
+    console.log( session, user );
+  }, [user, session])
     return (
         <>
             <News />
