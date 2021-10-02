142dffa7a7ddf1ed46b3994c12f4e9587b427f03
ArMinasyan <vip.arman.minasyan.197@gmail.com>
2021-10-02 19:22:32 +0400

    bug fixed (PG_Board data is deleted)

--- components/Pages/Personal/Content/PurchasedGames/Board/PGBoard/PG_board.js
+++ components/Pages/Personal/Content/PurchasedGames/Board/PGBoard/PG_board.js
@@ -7,270 +7,270 @@ import psjIcon from '/public/images/main/matches/psj.png'
 import rennaisIcon from '/public/images/main/matches/rennais.png'
 
 export const PG_board = [
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Барселона',
-        teamOneIcon: barcaIcon.src,
-        teamTwoName: 'ПСЖ',
-        teamTwoIcon: psjIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Марсель',
-        teamOneIcon: marcelIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: rennaisIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
-    {
-        teamOneName: 'Атлетико',
-        teamOneIcon: atleticoIcon.src,
-        teamTwoName: 'Атлетик',
-        teamTwoIcon: atleticIcon.src,
-        prediction: {
-            thereIs: false,
-            totalBet: 'тб 1,5 / ',
-            coefficent: 'коеф 3,5'
-        },
-        date: '14.01',
-        score: '1:0',
-        price: '1500 руб.'
-    },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Барселона',
+    //     teamOneIcon: barcaIcon.src,
+    //     teamTwoName: 'ПСЖ',
+    //     teamTwoIcon: psjIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Марсель',
+    //     teamOneIcon: marcelIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: rennaisIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
+    // {
+    //     teamOneName: 'Атлетико',
+    //     teamOneIcon: atleticoIcon.src,
+    //     teamTwoName: 'Атлетик',
+    //     teamTwoIcon: atleticIcon.src,
+    //     prediction: {
+    //         thereIs: false,
+    //         totalBet: 'тб 1,5 / ',
+    //         coefficent: 'коеф 3,5'
+    //     },
+    //     date: '14.01',
+    //     score: '1:0',
+    //     price: '1500 руб.'
+    // },
 ]
